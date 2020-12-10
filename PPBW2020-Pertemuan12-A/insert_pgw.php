d5f0b07f1c54652a1f44ddb34428804b36d32195
Derwi Rainord Nalle <71749127+Derwirainord@users.noreply.github.com>
2020-12-11 01:32:27 +0800

    Delete insert_pgw.php

--- PPBW2020-Pertemuan12-A/insert_pgw.php
+++
@@ -1,8 +0,0 @@
-<?php 
-include 'config/config.php';
-$nim = $_POST['nim'];
-$nama = $_POST['nama'];
-$alamat = $_POST['alamat'];
-mysqli_query($conn, "INSERT INTO mahasiswa VALUES('','$nim','$nama','$alamat','')");
-echo "<script>alert('Data Telah Berhasil Ditambahkan !!');location='pegawai.php';</script>";
-?>
