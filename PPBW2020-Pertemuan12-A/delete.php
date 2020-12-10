ba899ce29f65f9f141db467ffec52b563b098c69
Derwi Rainord Nalle <71749127+Derwirainord@users.noreply.github.com>
2020-12-11 01:29:20 +0800

    Delete delete.php

--- PPBW2020-Pertemuan12-A/delete.php
+++
@@ -1,6 +0,0 @@
-<?php
-include 'config/config.php';
-$id = $_GET['id'];
-mysqli_query($conn,"DELETE FROM mahasiswa WHERE id='$id'");
-echo "<script>alert('Data Telah Berhasil Dihapus!');location='admin.php';</script>";
-?>
